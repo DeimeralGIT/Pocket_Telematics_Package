bfd85f0f0cd96c7fa2f34b74658e7ec466da4ba7
DeimeralGIT <vahanhekimyan99@gmail.com>
2022-08-08 19:35:47 +0400

    added service termination bool hot fix

--- lib/pocket_telematics.dart
+++ lib/pocket_telematics.dart
@@ -10,38 +10,36 @@ import 'package:pocket_telematics/global_values.dart';
 
 class PocketTelematics {
   requestService() async {
-    await Geolocator.requestPermission().then((pass) {
-      //initial notification status + request permission
-      FlutterBackground.initialize(
-        androidConfig: const FlutterBackgroundAndroidConfig(
-          notificationTitle: "Not driving",
-          notificationText: "Driving tracking",
-          notificationImportance: AndroidNotificationImportance.Default,
-        ),
-      ).then(
-        (result) => FlutterBackground.enableBackgroundExecution().then((pass) {
-          //triggers on every GPS location update
-          positionStream = Geolocator.getPositionStream(
-              locationSettings: const LocationSettings(
-            accuracy: LocationAccuracy.bestForNavigation,
-          )).listen((position) => positionUpdate(position));
+    //initial notification status + request permission
+    FlutterBackground.initialize(
+      androidConfig: const FlutterBackgroundAndroidConfig(
+        notificationTitle: "Not driving",
+        notificationText: "Driving tracking",
+        notificationImportance: AndroidNotificationImportance.Default,
+      ),
+    ).then((result) => FlutterBackground.enableBackgroundExecution().then((pass) {
+          Geolocator.requestPermission().then((pass) {
+            //triggers on every GPS location update
+            positionStream = Geolocator.getPositionStream(
+                locationSettings: const LocationSettings(
+              accuracy: LocationAccuracy.bestForNavigation,
+            )).listen((position) => positionUpdate(position));
 
-          //Periodic function is for time-depending calculations
-          Timer.periodic(const Duration(milliseconds: 100), (t) {
-            //function is required only when driving is detected
-            if (activePeriodic) {
-              periodic();
-            }
+            //Periodic function is for time-depending calculations
+            Timer.periodic(const Duration(milliseconds: 100), (t) {
+              //function is required only when driving is detected
+              if (activePeriodic) {
+                periodic();
+              }
+            });
           });
-        }),
-      );
-    });
+        }));
   }
+}
 
-  terminateService() async {
-    FlutterBackground.disableBackgroundExecution();
-    try {
-      positionStream.cancel();
-    } catch (e) {}
-  }
+terminateService() async {
+  FlutterBackground.disableBackgroundExecution();
+  try {
+    positionStream.cancel();
+  } catch (e) {}
 }
