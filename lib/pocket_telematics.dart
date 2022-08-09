ab1c1ed80973244120ac43170b5dfdf4e311f83c
DeimeralGIT <vahanhekimyan99@gmail.com>
2022-08-09 13:51:37 +0400

    changed how package requests permissions

--- lib/pocket_telematics.dart
+++ lib/pocket_telematics.dart
@@ -10,37 +10,37 @@ import 'package:pocket_telematics/Methods/position_update.dart';
 import 'package:pocket_telematics/global_values.dart';
 
 class PocketTelematics {
-  Future<bool> requestService() async => Permission.locationWhenInUse.request().then((locWhenInUse) {
-        Permission.locationAlways.request().then((locAlways) {
-          Permission.ignoreBatteryOptimizations.request().then((batteryOptimisation) {
-            //initial notification status + request permission
-            FlutterBackground.initialize(
-              androidConfig: const FlutterBackgroundAndroidConfig(
-                notificationTitle: "Not driving",
-                notificationText: "Driving tracking",
-                notificationImportance: AndroidNotificationImportance.Default,
-              ),
-            ).then((result) {
-              FlutterBackground.enableBackgroundExecution().then((pass) {
-                //triggers on every GPS location update
-                positionStream = Geolocator.getPositionStream(
-                    locationSettings: const LocationSettings(
-                  accuracy: LocationAccuracy.bestForNavigation,
-                )).listen((position) => positionUpdate(position));
+  Future<bool> requestService() async => Permission.locationWhenInUse.request().then(
+        (locWhenInUse) => Permission.locationAlways.request().then(
+              (locAlways) => Permission.ignoreBatteryOptimizations.request().then((batteryOptimisation) {
+                //initial notification status + request permission
+                FlutterBackground.initialize(
+                  androidConfig: const FlutterBackgroundAndroidConfig(
+                    notificationTitle: "Not driving",
+                    notificationText: "Driving tracking",
+                    notificationImportance: AndroidNotificationImportance.Default,
+                  ),
+                ).then((result) {
+                  FlutterBackground.enableBackgroundExecution().then((pass) {
+                    //triggers on every GPS location update
+                    positionStream = Geolocator.getPositionStream(
+                        locationSettings: const LocationSettings(
+                      accuracy: LocationAccuracy.bestForNavigation,
+                    )).listen((position) => positionUpdate(position));
 
-                //Periodic function is for time-depending calculations
-                Timer.periodic(const Duration(milliseconds: 100), (t) {
-                  //function is required only when driving is detected
-                  if (activePeriodic) {
-                    periodic();
-                  }
+                    //Periodic function is for time-depending calculations
+                    Timer.periodic(const Duration(milliseconds: 100), (t) {
+                      //function is required only when driving is detected
+                      if (activePeriodic) {
+                        periodic();
+                      }
+                    });
+                  });
                 });
-              });
-            });
-            return locWhenInUse.isGranted && locAlways.isGranted && batteryOptimisation.isGranted;
-          });
-        });
-      });
+                return locWhenInUse.isGranted && locAlways.isGranted && batteryOptimisation.isGranted;
+              }),
+            ),
+      );
 
   terminateService() async {
     FlutterBackground.disableBackgroundExecution();
