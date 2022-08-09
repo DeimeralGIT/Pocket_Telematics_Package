84a3e4f4fa45770c0b964ca2ef22354ac2e9c4bf
DeimeralGIT <vahanhekimyan99@gmail.com>
2022-08-09 17:42:31 +0400

    changed how package requests permissions\

--- lib/pocket_telematics.dart
+++ lib/pocket_telematics.dart
@@ -33,26 +33,28 @@ class PocketTelematics {
         notificationText: "Driving tracking",
         notificationImportance: AndroidNotificationImportance.Default,
       ),
-    ).then(
-      (flutterBackInitialized) => FlutterBackground.enableBackgroundExecution().then(
-        (pass) {
-          //triggers on every GPS location update
-          positionStream = Geolocator.getPositionStream(
-              locationSettings: const LocationSettings(
-            accuracy: LocationAccuracy.bestForNavigation,
-          )).listen((position) => positionUpdate(position));
+    ).then((flutterBackInitialized) {
+      return flutterBackInitialized
+          ? FlutterBackground.enableBackgroundExecution().then(
+              (pass) {
+                //triggers on every GPS location update
+                positionStream = Geolocator.getPositionStream(
+                    locationSettings: const LocationSettings(
+                  accuracy: LocationAccuracy.bestForNavigation,
+                )).listen((position) => positionUpdate(position));
 
-          //Periodic function is for time-depending calculations
-          Timer.periodic(const Duration(milliseconds: 100), (t) {
-            //function is required only when driving is detected
-            if (activePeriodic) {
-              periodic();
-            }
-          });
+                //Periodic function is for time-depending calculations
+                Timer.periodic(const Duration(milliseconds: 100), (t) {
+                  //function is required only when driving is detected
+                  if (activePeriodic) {
+                    periodic();
+                  }
+                });
 
-          return true;
-        },
-      ),
-    );
+                return true;
+              },
+            )
+          : checkPermissions();
+    });
   }
 }
