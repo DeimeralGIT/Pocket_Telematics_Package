2c2dbf52e2463acac56cb1730fd571872599d02c
DeimeralGIT <vahanhekimyan99@gmail.com>
2022-08-09 15:41:46 +0400

    changed how package requests permissions

--- lib/pocket_telematics.dart
+++ lib/pocket_telematics.dart
@@ -15,36 +15,8 @@ class PocketTelematics {
         (locWhenInUse) => Permission.locationAlways.request().then((locAlways) {
           return locAlways.isGranted
               ? Permission.ignoreBatteryOptimizations.request().then((batteryPermission) {
-                  return batteryPermission.isGranted
-                      ? //initial notification status + request permission
-                      FlutterBackground.initialize(
-                          androidConfig: const FlutterBackgroundAndroidConfig(
-                            notificationTitle: "Not driving",
-                            notificationText: "Driving tracking",
-                            notificationImportance: AndroidNotificationImportance.Default,
-                          ),
-                        ).then(
-                          (flutterBackInitialized) => FlutterBackground.enableBackgroundExecution().then(
-                            (pass) {
-                              //triggers on every GPS location update
-                              positionStream = Geolocator.getPositionStream(
-                                  locationSettings: const LocationSettings(
-                                accuracy: LocationAccuracy.bestForNavigation,
-                              )).listen((position) => positionUpdate(position));
-
-                              //Periodic function is for time-depending calculations
-                              Timer.periodic(const Duration(milliseconds: 100), (t) {
-                                //function is required only when driving is detected
-                                if (activePeriodic) {
-                                  periodic();
-                                }
-                              });
-
-                              return true;
-                            },
-                          ),
-                        )
-                      : checkPermissions();
+                  log(batteryPermission.isGranted.toString());
+                  return batteryPermission.isGranted ? _startBackgroundService() : checkPermissions();
                 })
               : checkPermissions();
         }),
@@ -59,4 +31,35 @@ class PocketTelematics {
 
   Future<bool> checkPermissions() async =>
       Permission.locationAlways.isGranted.then((locationPermission) => locationPermission && FlutterBackground.isBackgroundExecutionEnabled);
+
+  Future<bool> _startBackgroundService() {
+    //initial notification status + request permission
+    return FlutterBackground.initialize(
+      androidConfig: const FlutterBackgroundAndroidConfig(
+        notificationTitle: "Not driving",
+        notificationText: "Driving tracking",
+        notificationImportance: AndroidNotificationImportance.Default,
+      ),
+    ).then(
+      (flutterBackInitialized) => FlutterBackground.enableBackgroundExecution().then(
+        (pass) {
+          //triggers on every GPS location update
+          positionStream = Geolocator.getPositionStream(
+              locationSettings: const LocationSettings(
+            accuracy: LocationAccuracy.bestForNavigation,
+          )).listen((position) => positionUpdate(position));
+
+          //Periodic function is for time-depending calculations
+          Timer.periodic(const Duration(milliseconds: 100), (t) {
+            //function is required only when driving is detected
+            if (activePeriodic) {
+              periodic();
+            }
+          });
+
+          return true;
+        },
+      ),
+    );
+  }
 }
